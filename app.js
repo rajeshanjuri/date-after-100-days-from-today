96d818577da628d38456e409e033e10b4eb087db
rajeshanjuri <rajeshanjuri555@gmail.com>
2023-03-03 09:38:48 +0000

    100-days

---
+++ app.js
@@ -0,0 +1,16 @@
+const addDays = require("date-fns/addDays");
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  let date = new Date();
+  let result = addDays(
+    new Date(date.getFullYear(), date.getMonth(), date.getDate()),
+    100
+  );
+  response.send(
+    `${result.getDate()}/${result.getMonth() + 1}/${result.getFullYear()}`
+  );
+});
+app.listen(3000);
+module.exports = app;
